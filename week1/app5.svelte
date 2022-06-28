57ef214c7b6fffe37afbb3f86143f982af31e3bc
jyooj08 <jyooj08@naver.com>
2022-06-28 16:46:35 +0900

    이벤트 핸들링

---
+++ week1/app5.svelte
@@ -0,0 +1,15 @@
+<script>
+	let text = '';
+</script>
+
+<h1>
+	{text}
+</h1>
+<input type='text' 
+			 value={text}
+			 on:input={(e)=>{text = e.target.value}}/>
+<input type='text'
+			 bind:value={text}/>
+<button on:click={()=>{text='Yujin'}}>
+	click
+</button>
