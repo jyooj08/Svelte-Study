289a5fa2173b72fc340e41b1ba79e24f4a851f3a
jyooj08 <jyooj08@naver.com>
2022-06-28 16:47:13 +0900

    컴포넌트

---
+++ week1/Fruits.svelte
@@ -0,0 +1,27 @@
+<script>
+	// props
+	export let fruits;
+	export let reverse;
+	export let slice;
+	let computedFruits = [];
+	let name = ''
+	
+	if(reverse) {
+		computedFruits = [...fruits].reverse();	
+		name = 'reverse';
+	} else if (slice) {
+		computedFruits = fruits.slice(...slice.split(','));
+		name = 'slice ' + slice
+	} else {
+		computedFruits = fruits;
+	}
+</script>
+
+<h2>
+	Fruits {name}
+</h2>
+<ul>
+	{#each computedFruits as f}
+		<li>{f}</li>
+	{/each}
+</ul>
