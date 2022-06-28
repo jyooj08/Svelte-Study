1efbfd8e6e4c690ff1001047fafb5208db54f21a
jyooj08 <jyooj08@naver.com>
2022-06-28 16:44:07 +0900

    선언적 렌더링

---
+++ week1/app1.svelte
@@ -0,0 +1,27 @@
+<script>
+	let name = 'world';
+	let age = 80;
+	function assign(){
+		name = 'yujin';
+		age = 25;
+	}
+</script>
+
+<h1>Hello {name}!</h1>
+<h2 class={age < 85 ? 'active':''}>
+	{age}
+</h2>
+<img src="" alt={name} />
+<input type="text" bind:value={name} />
+<button on:click={assign}>
+	Assign
+</button>
+
+<style>
+	h1{
+		color:red;
+	}
+	.active{
+		color:blue;
+	}
+</style>
