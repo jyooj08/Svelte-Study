fe84d0052d26f312d5720b6b427ed51d06daa6b3
jyooj08 <jyooj08@naver.com>
2022-06-28 16:48:03 +0900

    Todo 예제 만들기

---
+++ week1/SimpleTodoApp.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Todo from './Todo.svelte';
+	import {writable} from 'svelte/store';
+	let title = '';
+	let todos = writable([]);
+	let id = 0;
+	
+	function createTodo(){
+		if(!title.trim()){
+			title = '';
+			return;
+		}
+		$todos.push({
+			id,
+			title
+		});
+		$todos = $todos;
+		title = '';
+		id += 1;
+	}
+</script>
+
+<input bind:value={title} 
+			 type='text'
+			 on:keydown={(e) => {e.key === 'Enter' && createTodo();}}/>
+<button on:click={createTodo}> 
+	Create Todo
+</button>
+
+{#each $todos as todo}
+	<Todo {todos} {todo} />
+{/each}
