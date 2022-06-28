df68574e95931490e1b524e0c6fabbc6cb27392c
jyooj08 <jyooj08@naver.com>
2022-06-28 16:46:11 +0900

    조건문과 반복문

---
+++ week1/app2.svelte
@@ -0,0 +1,15 @@
+<script>
+	let name = 'world';
+	let toggle = false;
+</script>
+
+<button on:click={()=>{toggle = !toggle}}>
+	Toggle
+</button>
+{#if toggle}
+<h1>Hello {name}!</h1>
+{:else}
+<div>
+	No name!
+</div>
+{/if}
---
+++ week1/app3.svelte
@@ -0,0 +1,20 @@
+<script>
+	let name = 'fruits';
+	let fruits = ['apple', 'banana', 'cherry', 'orange', 'mango']
+	
+	function deleteFruit(){
+		fruits = fruits.slice(1);
+	}
+</script>
+
+<h1>Hello {name}!</h1>
+
+<ul>
+	{#each fruits as fruit}
+	<li>{fruit}</li>
+	{/each}
+</ul>
+
+<button on:click={deleteFruit}>
+	Eat it!
+</button>
