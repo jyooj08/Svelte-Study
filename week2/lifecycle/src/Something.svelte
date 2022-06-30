0efefad6c0f4f29f653446d05e7596a37a361193
jyooj08 <jyooj08@naver.com>
2022-06-30 15:13:45 +0900

    onMount, onDestroy

---
+++ week2/lifecycle/src/Something.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { onDestroy, onMount } from "svelte";
+
+  onMount(() => {
+    console.log("Mounted!");
+
+    // onMount가 반환하는 함수는 컴포넌트가 연결 해제될 때 실행됨. 즉 onDestory와 같은 역할. onDestroy보다 나중에 실행됨.
+    return () => {
+      console.log("Destroy in mount");
+    };
+  });
+
+  onDestroy(() => {
+    console.log("Before Destroy");
+    const h1 = document.querySelector("h1");
+    console.log(h1.innerText);
+  });
+</script>
+
+<h1>Something ...</h1>
