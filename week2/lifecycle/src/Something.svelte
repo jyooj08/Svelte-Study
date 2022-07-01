5afa2e45bd1a1c1f741be28323f4b10226210c83
jyooj08 <jyooj08@naver.com>
2022-07-01 11:23:14 +0900

    beforeUpdate, afterUpdate

--- week2/lifecycle/src/Something.svelte
+++ week2/lifecycle/src/Something.svelte
@@ -1,13 +1,31 @@
 <script>
-  import { onDestroy, onMount } from "svelte";
+  import { afterUpdate, beforeUpdate, onDestroy, onMount } from "svelte";
+
+  let name = "Something...";
+  let h1;
+
+  function moreDot() {
+    name += ".";
+  }
+
+  beforeUpdate(() => {
+    console.log("Before update!");
+    console.log(h1 && h1.innerText);
+  });
+
+  afterUpdate(() => {
+    console.log("After update!");
+    console.log(h1.innerText);
+  });
 
   onMount(() => {
     console.log("Mounted!");
+    h1 = document.querySelector("h1");
 
     // onMount가 반환하는 함수는 컴포넌트가 연결 해제될 때 실행됨. 즉 onDestory와 같은 역할. onDestroy보다 나중에 실행됨.
-    return () => {
-      console.log("Destroy in mount");
-    };
+    // return () => {
+    //   console.log("Destroy in mount");
+    // };
   });
 
   onDestroy(() => {
@@ -17,4 +35,4 @@
   });
 </script>
 
-<h1>Something ...</h1>
+<h1 on:click={moreDot}>{name}</h1>
