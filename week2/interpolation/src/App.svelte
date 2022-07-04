c02a3b8856936ad7004abedb140b0f07cbcbd517
jyooj08 <jyooj08@naver.com>
2022-07-04 14:16:14 +0900

    내용/속성/표현식 보간

---
+++ week2/interpolation/src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  let href = "https://velog.io/@jyooj08";
+  let name = "Yujin";
+  let value = "New input value!";
+  let isUpperCase = false;
+</script>
+
+<!-- <a href="https://velog.io/@jyooj08">Yujin</a> -->
+
+<a {href}>{name}</a>
+
+<!-- <input type="text" value="Default value.." /> -->
+
+<!-- input의 기본 type값은 text이므로 생략 가능 -->
+<input
+  {value}
+  on:input={(e) => {
+    value = e.target.value;
+  }}
+/>
+<input bind:value />
+
+<div>{isUpperCase ? "DIV" : "div"}</div>
