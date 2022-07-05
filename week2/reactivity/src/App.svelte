fe6e57dc66af2b008008418a759f7ab29938d889
jyooj08 <jyooj08@naver.com>
2022-07-05 17:13:29 +0900

    반응성 구문 패턴 정리

--- week2/reactivity/src/App.svelte
+++ week2/reactivity/src/App.svelte
@@ -1,22 +1,64 @@
 <script>
-  import { tick } from "svelte";
   let count = 0;
-  let double = 0;
 
+  // 선언
+  $: double = count;
+
+  // 블록
   $: {
-    double = count * 2;
-    console.log("double!");
+    console.log(count);
+    console.log(double);
   }
 
-  async function assign() {
+  // 함수 실행
+  $: count, log();
+
+  // 즉시 실행 함수(IIFE)
+  $: count,
+    (() => {
+      console.log("iife: Heropy");
+    })();
+
+  // 조건문(if)
+  $: if (count > 0) {
+    console.log("if: ", double);
+  }
+
+  // 반복문(for)
+  $: for (let i = 0; i < 3; i++) {
+    count;
+    console.log("for:", i);
+  }
+
+  // 조건문(Switch)
+  $: switch (count) {
+    case 1:
+      console.log("switch: 1");
+      break;
+    default:
+      console.log("switch: default");
+  }
+
+  // 유효범위
+  $: {
+    function scope1() {
+      console.log("scope1");
+      function scope2() {
+        console.log("scope2");
+        function scope3() {
+          console.log("scope3", count);
+        }
+      }
+    }
+  }
+
+  function log() {
+    console.log("fn: Heropy");
+  }
+
+  function assign() {
     count++;
-    console.time("timer");
-    await tick();
-    console.timeEnd("timer");
-    console.log(double);
   }
 </script>
 
 <button on:click={assign}>Assign!</button>
-<h2>{count}</h2>
-<h2>{double}</h2>
---
+++ week2/reactivity/src/App3.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { tick } from "svelte";
+  let count = 0;
+  let double = 0;
+
+  $: {
+    double = count * 2;
+    console.log("double!");
+  }
+
+  async function assign() {
+    count++;
+    console.time("timer");
+    await tick();
+    console.timeEnd("timer");
+    console.log(double);
+  }
+</script>
+
+<button on:click={assign}>Assign!</button>
+<h2>{count}</h2>
+<h2>{double}</h2>
