cf090e0750c92150a57bf5e405434aff63c55595
jyooj08 <jyooj08@naver.com>
2022-07-05 17:00:00 +0900

    반응성 구문의 이해

--- week2/reactivity/src/App.svelte
+++ week2/reactivity/src/App.svelte
@@ -1,24 +1,22 @@
 <script>
-  // 일반 javascript
-  loop1: for (let i = 0; i < 3; i++) {
-    loop2: for (let j = 0; j < 3; j++) {
-      if (i === 2) {
-        break loop1;
-      }
-      console.log(`${i}-${j}`);
-    }
-    console.log("---------");
-  }
-
-  heropy: {
-    console.log("a");
-    break heropy;
-    console.log("b");
-  }
-
-  // svelte 반응성 구문
+  import { tick } from "svelte";
   let count = 0;
+  let double = 0;
+
   $: {
-    console.log(count);
+    double = count * 2;
+    console.log("double!");
+  }
+
+  async function assign() {
+    count++;
+    console.time("timer");
+    await tick();
+    console.timeEnd("timer");
+    console.log(double);
   }
 </script>
+
+<button on:click={assign}>Assign!</button>
+<h2>{count}</h2>
+<h2>{double}</h2>
---
+++ week2/reactivity/src/App2.svelte
@@ -0,0 +1,24 @@
+<script>
+  // 일반 javascript
+  loop1: for (let i = 0; i < 3; i++) {
+    loop2: for (let j = 0; j < 3; j++) {
+      if (i === 2) {
+        break loop1;
+      }
+      console.log(`${i}-${j}`);
+    }
+    console.log("---------");
+  }
+
+  heropy: {
+    console.log("a");
+    break heropy;
+    console.log("b");
+  }
+
+  // svelte 반응성 구문
+  let count = 0;
+  $: {
+    console.log(count);
+  }
+</script>
