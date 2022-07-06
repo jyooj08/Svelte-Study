2e3516439f417a5cd1eda7da2f85177d9043adc0
jyooj08 <jyooj08@naver.com>
2022-07-06 13:50:35 +0900

    클래스 바인딩 패턴 정리

---
+++ week3/ClassAndStyle/src/App1.svelte
@@ -0,0 +1,36 @@
+<script>
+  let active = false;
+  let color = {
+    t: "tomato",
+    w: "#fff",
+  };
+  let letterSpacing = "letter-spacing: 5px;";
+</script>
+
+<button on:click={() => (active = !active)}> Toggle! </button>
+
+<!-- <div class={active ? "active" : ""}>Hello</div> -->
+<div class:active>Hello</div>
+
+<h2 style="background-color:{color.t}; color: {color.w}; {letterSpacing}">
+  Heropy!
+</h2>
+
+<style>
+  div {
+    width: 120px;
+    height: 200px;
+    background: royalblue;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    font-size: 20px;
+    transition: 0.4s;
+  }
+  .active {
+    width: 250px;
+    background: tomato;
+  }
+</style>
