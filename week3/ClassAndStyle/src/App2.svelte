edd5d41c54664dc957f059ba9f9381e83d3593c7
jyooj08 <jyooj08@naver.com>
2022-07-06 16:56:55 +0900

    스타일 유효범위(hash)와 전역화(global)

---
+++ week3/ClassAndStyle/src/App2.svelte
@@ -0,0 +1,21 @@
+<script>
+  let active = true;
+  let valid = false;
+  let camelCase = true;
+
+  function multi() {
+    return "active multiple-class";
+  }
+</script>
+
+<div class={active ? "active" : ""}>3항 연산자 보간</div>
+
+<div class:hello={active}>Class 지시어 (directive) 바인딩</div>
+
+<div class:active>Class 지시어 바인딩 단축 형태</div>
+
+<div class:active class:valid class:camelCase class:camel-case={camelCase}>
+  다중(multiple) Class 지시어 바인딩
+</div>
+
+<div class={multi()}>함수 실행</div>
