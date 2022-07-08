2268c08a453111386b7f4b35d81ba795313ff79f
jyooj08 <jyooj08@naver.com>
2022-07-08 15:10:12 +0900

    조건 블록 패턴 정리

---
+++ week3/ConditionAndRepeat/src/App.svelte
@@ -0,0 +1,54 @@
+<script>
+  let count = 0;
+</script>
+
+<button
+  on:click={() => {
+    count++;
+  }}>증가</button
+>
+<button
+  on:click={() => {
+    count--;
+  }}>감소</button
+>
+
+<h2>{count}</h2>
+
+<section>
+  <h2>if</h2>
+  {#if count > 3}
+    <div>count &gt; 3</div>
+  {/if}
+</section>
+
+<section>
+  <h2>if else</h2>
+  {#if count > 3}
+    <div>count &gt; 3</div>
+  {:else}
+    <div>count &lt;= 3</div>
+  {/if}
+</section>
+
+<section>
+  <h2>if else if</h2>
+  {#if count > 3}
+    <div>count &gt; 3</div>
+  {:else if count === 3}
+    <div>count === 3</div>
+  {:else}
+    <div>count &lt; 3</div>
+  {/if}
+</section>
+
+<section>
+  <h2>다중 블록</h2>
+  {#if count > 3}
+    {#if count === 5}
+      count === 5
+    {:else}
+      count &gt; 3
+    {/if}
+  {/if}
+</section>
