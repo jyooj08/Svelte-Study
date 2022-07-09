0683d377d11b3a0d37e9ee036e09dc44d65691e3
jyooj08 <jyooj08@naver.com>
2022-07-09 14:55:18 +0900

    반복 블록 패턴 정리

---
+++ week3/ConditionAndRepeat/src/App2.svelte
@@ -0,0 +1,21 @@
+<script>
+  // let fruits = ["Apple", "Banana", "Cherry", "Orange"];
+  let fruits = [
+    { id: "1", name: "Apple" },
+    { id: "2", name: "Banana" },
+    { id: "3", name: "Cherry" },
+    { id: "4", name: "Apple" },
+  ];
+
+  function deleteFirst() {
+    fruits = fruits.slice(1);
+  }
+</script>
+
+<button on:click={deleteFirst}> Delete first fruit!</button>
+
+<ul>
+  {#each fruits as fruit (fruit.id)}
+    <li>{fruit.name}</li>
+  {/each}
+</ul>
