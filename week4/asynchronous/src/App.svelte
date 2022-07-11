5ebedf8674e6043eb9dd6a17476a35a20eefec6d
jyooj08 <jyooj08@naver.com>
2022-07-11 10:28:57 +0900

    JS 비동기 처리의 이해와 사용 패턴 정리(1)

---
+++ week4/asynchronous/src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+  function a() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log("a");
+        resolve();
+      }, 1000);
+    });
+  }
+
+  function b() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log("b");
+        resolve();
+      }, 1000);
+    });
+  }
+
+  function c() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log("c");
+        resolve();
+      }, 1000);
+    });
+  }
+
+  function d() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log("d");
+        resolve();
+      }, 1000);
+    });
+  }
+
+  a()
+    .then(() => b())
+    .then(() => c())
+    .then(() => d());
+</script>
