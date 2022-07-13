08d733846d3274dcdda41ab603c8b6670ed38e1f
jyooj08 <jyooj08@naver.com>
2022-07-13 16:02:39 +0900

    영화 검색 API 예제 만들기

---
+++ week4/movie/src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+  import axios from "axios";
+
+  let apikey = "9d38c929";
+  let title = "";
+  let movies = null;
+  let error = null;
+  let loading = false;
+
+  async function searchMovies() {
+    if (loading) return;
+    movies = null;
+    error = null;
+    loading = true;
+    try {
+      const res = await axios.get(
+        `http://www.omdbapi.com/?apikey=${apikey}&s=${title}`
+      );
+      console.log(res);
+      movies = res.data.Search;
+    } catch (err) {
+      console.log(err.message);
+      error = err;
+    } finally {
+      loading = false;
+    }
+  }
+</script>
+
+<input bind:value={title} />
+<button on:click={searchMovies}>검색</button>
+
+{#if loading}
+  <p style="color: royalblue">Loading...</p>
+{:else if movies}
+  <ul>
+    {#each movies as movie}
+      <li>{movie.Title}</li>
+    {/each}
+  </ul>
+{:else if error}
+  <p style="color:red">{error.message}</p>
+{/if}
