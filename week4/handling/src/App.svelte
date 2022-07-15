8a2d9fd7083a5e7d607cfba11bcf6a3a2e4000dd
jyooj08 <jyooj08@naver.com>
2022-07-15 13:56:19 +0900

    이벤트 수식어

--- week4/handling/src/App.svelte
+++ week4/handling/src/App.svelte
@@ -1,19 +1,65 @@
 <script>
-  let count = 0;
-
-  function increase() {
-    count++;
+  function clickHandler(event) {
+    console.log(event.currentTarget);
   }
 
-  function current(e) {
-    console.log(e.currentTarget);
+  function wheelHandler(event) {
+    console.log(event);
   }
 </script>
 
-<button
-  on:click={increase}
-  on:click={current}
-  on:click={() => console.log("click!")}>Click me!</button
->
+<section>
+  <!-- 기본 동작 방지 -->
+  <h2>preventDefault</h2>
+  <a
+    href="https://naver.com"
+    target="_blank"
+    on:click|preventDefault={clickHandler}
+  >
+    Naver</a
+  >
+</section>
 
-<h1>{count}</h1>
+<section>
+  <!-- 최초 실행 후 핸들러 삭제 -->
+  <h2>Once</h2>
+  <a
+    href="https://naver.com"
+    target="_blank"
+    on:click|preventDefault|once={clickHandler}
+  >
+    Naver</a
+  >
+</section>
+
+<section>
+  <!-- 이벤트 버빌링 방지 -->
+  <h2>stopPropagation</h2>
+  <div class="parent" on:click={clickHandler}>
+    <div class="child" on:click|stopPropagation={clickHandler} />
+  </div>
+</section>
+
+<section>
+  <!-- 캡처링에서 핸들러 실행 -->
+  <h2>capture</h2>
+  <div class="parent" on:click|capture={clickHandler}>
+    <div class="child" on:click={clickHandler} />
+  </div>
+</section>
+
+<section>
+  <!-- event의 target과 currentTarget이 일치하는 경우 핸들러 실행 -->
+  <h2>self</h2>
+  <div class="parent" on:click|self={clickHandler}>
+    <div class="child" />
+  </div>
+</section>
+
+<section>
+  <!-- 이벤트 처리를 완료하지 않고도 기본 속도로 화면을 스크롤 -->
+  <h2>passive</h2>
+  <div class="parent wheel" on:wheel|passive={wheelHandler}>
+    <div class="child" />
+  </div>
+</section>
---
+++ week4/handling/src/App2.svelte
@@ -0,0 +1,19 @@
+<script>
+  let count = 0;
+
+  function increase() {
+    count++;
+  }
+
+  function current(e) {
+    console.log(e.currentTarget);
+  }
+</script>
+
+<button
+  on:click={increase}
+  on:click={current}
+  on:click={() => console.log("click!")}>Click me!</button
+>
+
+<h1>{count}</h1>
