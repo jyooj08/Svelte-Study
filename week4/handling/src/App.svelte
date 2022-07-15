766c5daf519f121062b2218649e5d7e3ea35f99a
jyooj08 <jyooj08@naver.com>
2022-07-15 11:12:04 +0900

    인라인 이벤트 핸들러

---
+++ week4/handling/src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+  let fruits = [
+    { id: 1, name: "Apple" },
+    { id: 2, name: "Banana" },
+    { id: 3, name: "Cherry" },
+  ];
+
+  function assign(fruit) {
+    fruit.name += "!";
+    fruits = fruits;
+  }
+</script>
+
+<section>
+  {#each fruits as fruit (fruit.id)}
+    <div on:click={() => assign(fruit)}>
+      {fruit.name}
+    </div>
+  {/each}
+</section>
+
+<section>
+  {#each fruits as fruit (fruit.id)}
+    <div on:click={() => (fruit.name += "!")}>
+      {fruit.name}
+    </div>
+  {/each}
+</section>
+
+<section>
+  {#each fruits as { id, name } (id)}
+    <div on:click={() => (name += "!")}>
+      {name}
+    </div>
+  {/each}
+</section>
