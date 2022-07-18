911a31640ff6b9014ce43e28a0dd400727ff7ca9
jyooj08 <jyooj08@naver.com>
2022-07-18 14:18:34 +0900

    컴포넌트 개요

---
+++ week5/component/src/Heropy.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let title = "Hello";
+
+  let name = "Heropy";
+  let age = 85;
+  let email = "aaa@gmail.com";
+</script>
+
+<h2>{title}</h2>
+<div>{name}</div>
+<div>{age}</div>
+<div>{email}</div>
