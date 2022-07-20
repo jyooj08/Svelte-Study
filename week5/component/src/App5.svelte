804af5c50b44302368ff42f75fb50e52c4a56a29
jyooj08 <jyooj08@naver.com>
2022-07-20 11:04:43 +0900

    오디오플레이어 예제

---
+++ week5/component/src/App5.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Fruit, { count } from "./Fruit.svelte";
+
+  let fruits = ["Apple", "Banana", "Cherry", "Mango", "Orange"];
+</script>
+
+<button
+  on:click={() => {
+    console.log(count);
+  }}>Total count log!</button
+>
+
+{#each fruits as fruit}
+  <Fruit {fruit} />
+{/each}
