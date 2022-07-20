3e6d34c598e22552b1b4290e84ca0a762fac7b47
jyooj08 <jyooj08@naver.com>
2022-07-20 16:13:46 +0900

    단일 슬롯과 fallback content

---
+++ week5/slot/src/Btn.svelte
@@ -0,0 +1,33 @@
+<script>
+  export let block;
+  export let color;
+</script>
+
+<button
+  class:block
+  style="background-color: {color}; color: {color ? 'white' : ''};"
+>
+  <slot>Default Button!</slot>
+</button>
+
+<style>
+  button {
+    background: lightgray;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 10px;
+    cursor: pointer;
+    transition: 0.2s;
+    margin-bottom: 10px;
+  }
+  button:hover {
+    text-decoration: underline;
+  }
+  button:active {
+    transform: scale(1.05);
+  }
+  button.block {
+    width: 100%;
+    display: block;
+  }
+</style>
