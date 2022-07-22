4e091a99fa95e98a28077c0e874a46ec44942c5a
jyooj08 <jyooj08@naver.com>
2022-07-22 11:17:06 +0900

    3605slots

---
+++ week5/slot/src/UserCard.svelte
@@ -0,0 +1,25 @@
+<script>
+  console.log($$slots);
+</script>
+
+<div class="user-card">
+  <slot name="name" />
+  {#if $$slots.age}
+    <hr />
+    <slot name="age" />
+  {/if}
+  {#if $$slots.email}
+    <hr />
+    <slot name="email" />
+  {/if}
+</div>
+
+<style>
+  .user-card {
+    width: 300px;
+    margin: 20px 10px;
+    padding: 0 10px 20px;
+    border: 4px solid lightgray;
+    border-radius: 10px;
+  }
+</style>
