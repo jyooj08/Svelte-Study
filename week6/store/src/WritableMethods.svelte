47ad2ba9a0cc865a83f68c30ad8a3e48e4e7a08e
jyooj08 <jyooj08@naver.com>
2022-07-25 16:48:29 +0900

    쓰기 가능 스토어, 수동 구독과 자동 구독

---
+++ week6/store/src/WritableMethods.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { onDestroy } from "svelte";
+
+  import { count, name } from "./store";
+
+  //   let number, userName;
+  //   const unsubscribeCount = count.subscribe((c) => {
+  //     number = c;
+  //   });
+  //   const unsubscribeCount2 = count.subscribe(() => {});
+  //   const unsubscribeName = name.subscribe((n) => {
+  //     userName = n;
+  //   });
+
+  function increase() {
+    $count++;
+  }
+
+  function changeName() {
+    $name = "Neo";
+  }
+
+  //   onDestroy(() => {
+  //     unsubscribeCount();
+  //     unsubscribeCount2();
+  //   });
+</script>
+
+<button on:click={increase} on:click={changeName}> Click me! </button>
+
+<h2>{$count}</h2>
+<h2>{$name}</h2>
