a96e4e5cfcd6c813cffb9201abed1a5a27f6d781
jyooj08 <jyooj08@naver.com>
2022-07-26 16:57:07 +0900

    읽기 전용 스토어

---
+++ week6/store/src/Readable.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { user } from "./store2";
+
+  console.log(user);
+  console.log($user);
+</script>
+
+<!-- error -->
+<button
+  on:click={() => {
+    $user.name = "Neo";
+  }}>Click!</button
+>
+
+<h2>{$user.name}</h2>
