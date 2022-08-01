a4b5b46d2122023d932152be17802938374a8d45
jyooj08 <jyooj08@naver.com>
2022-08-01 13:52:42 +0900

    커스텀 스토어

--- week6/store/src/App.svelte
+++ week6/store/src/App.svelte
@@ -1,8 +1,15 @@
 <script>
-  import { count, double, user } from "./store4";
-  import { get } from "svelte/store";
+  import { fruits } from "./fruits";
 
-  console.log(get(count));
-  console.log(get(double));
-  console.log(get(user));
+  let value;
 </script>
+
+<input bind:value />
+<button on:click={() => fruits.setItem(value)}>Add fruit!</button>
+<button on:click={() => console.log(fruits.getList())}>Log fruit list!</button>
+
+<ul>
+  {#each $fruits as { id, name } (id)}
+    <li>{name}</li>
+  {/each}
+</ul>
---
+++ week6/store/src/App4.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { count, double, user } from "./store4";
+  import { get } from "svelte/store";
+
+  console.log(get(count));
+  console.log(get(double));
+  console.log(get(user));
+</script>
