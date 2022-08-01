b41ebb4a875e7f677b162b491bb7bc08e9655862
jyooj08 <jyooj08@naver.com>
2022-08-01 14:24:07 +0900

    사용 패턴 정리

---
+++ week6/action/src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+  let toggle = true;
+  let width = 200;
+  function hello(node, options = {}) {
+    const { width, height, color } = options;
+    console.log(node);
+    node.style.width = width || "100px";
+    node.style.height = height || "100px";
+    node.style.backgroundColor = color || "tomato";
+
+    return {
+      update(opts) {
+        console.log("update!", opts);
+        node.style.width = opts.width;
+      },
+      destroy() {
+        console.log("destroy!");
+      },
+    };
+  }
+</script>
+
+<button on:click={() => (toggle = !toggle)}>Toggle</button>
+<button on:click={() => (width += 20)}>Size up</button>
+
+<div use:hello />
+
+{#if toggle}
+  <div
+    use:hello={{ width: `${width}px`, height: "70px", color: "royalblue" }}
+  />
+{/if}
