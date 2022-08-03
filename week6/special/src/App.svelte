28f37fddfca0dd25de585627dc71709f5245b927
jyooj08 <jyooj08@naver.com>
2022-08-04 08:28:46 +0900

    head, body

--- week6/special/src/App.svelte
+++ week6/special/src/App.svelte
@@ -1,41 +1,11 @@
 <script>
-  let key = "";
-  let innerWidth;
-  let innerHeight;
-  let outerWidth;
-  let outerHeight;
-  let online;
-  let scrollX;
-  let scrollY;
+  import Heropy from "./Heropy.svelte";
 
-  // window.addEventListener("keydown", (event) => {
-  //   key = event.key;
-  // });
+  let toggle = false;
 </script>
 
-<svelte:window
-  on:keydown={(e) => (key = e.key)}
-  bind:innerWidth
-  bind:innerHeight
-  bind:outerWidth
-  bind:outerHeight
-  bind:online
-  bind:scrollX
-  bind:scrollY
-/>
+<button on:click={() => (toggle = !toggle)}>Toggle!</button>
 
-<h1>{key}</h1>
-<div>innerWidth: {innerWidth}</div>
-<div>innerHeight: {innerHeight}</div>
-<div>outerWidth: {outerWidth}</div>
-<div>outerHeight: {outerHeight}</div>
-<div>online: {online}</div>
-<input type="number" bind:value={scrollX} />
-<input type="number" bind:value={scrollY} />
-<div class="for-scroll" />
-
-<style>
-  .for-scroll {
-    height: 2000px;
-  }
-</style>
+{#if toggle}
+  <Heropy />
+{/if}
--- week6/special/src/Heropy.svelte
+++ week6/special/src/Heropy.svelte
@@ -1,5 +1,10 @@
-<script>
-  export let index;
-</script>
+<!-- document.head -->
+<svelte:head>
+  <!-- runtime에 추가되기 때문에 상대경로 사용하면 해석할 수 없음, index.html 주변에 작성해야 함 -->
+  <link rel="stylesheet" href="./main.css" />
+</svelte:head>
 
-<h2>{index}. Heropy</h2>
+<!-- document.body -->
+<svelte:body on:mousemove={(e) => console.log(e.clientX, e.clientY)} />
+
+<h1>Heropy</h1>
