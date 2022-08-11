a29c93ddaeaa9baddbb9aa573e7bb33c601aa107
jyooj08 <jyooj08@naver.com>
2022-08-11 13:59:01 +0900

    SCSS

--- Trello/src/components/Header.svelte
+++ Trello/src/components/Header.svelte
@@ -2,17 +2,17 @@
   <img src="/images/trello-logo.svg" alt="Trello" class="logo" />
 </header>
 
-<style>
+<style lang="scss">
   header {
     height: 40px;
     background-color: rgba(0, 0, 0, 0.4);
     display: flex;
     justify-content: center;
     align-items: center;
-  }
 
-  header img.logo {
-    width: 80px;
-    opacity: 0.5;
+    img.logo {
+      width: 80px;
+      opacity: 0.5;
+    }
   }
 </style>
